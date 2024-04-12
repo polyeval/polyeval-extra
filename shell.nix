782f266eebd7d940d169a199ee0baf806de67d23
hzfsls <2508342095@qq.com>
2024-04-12 15:34:00 +0800

    update: submodules

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
   pkgs = import nixpkgs { config = {}; overlays = []; };
   executionEnv = import ./execution-templates/shell.nix ;
 in
